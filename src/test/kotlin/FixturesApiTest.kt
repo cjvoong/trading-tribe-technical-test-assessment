d22b3fbfee0ed7ed146c8c9e2b66392db13b5092
Chi Voong <chi.voong@skybettingandgaming.com>
2019-05-21 16:15:07 +0100

    added test

--- src/test/kotlin/FixturesApiTest.kt
+++ src/test/kotlin/FixturesApiTest.kt
@@ -30,12 +30,13 @@ class FixturesApiTest {
     @Test
     fun testEachFixtureHasFixtureId(){
         val fixtureIds = RestAssured.given().expect().statusCode(200).`when`().get("fixtures").then().extract().response().jsonPath().getList<Int>("fixtureId")
-        assertEquals(3,fixtureIds.size)
+        assertEquals(4,fixtureIds.size)
     }
 
     @Test
     fun testGetNewFixture(){
-
+        val teamId = RestAssured.given().expect().statusCode(200).`when`().get("fixture/4").then().extract().response().jsonPath().getString("footballFullState.teams[0].teamId")
+        assertEquals("HOME",teamId)
     }
 
     @Test
